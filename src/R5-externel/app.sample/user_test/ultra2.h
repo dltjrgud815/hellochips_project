5bdc8ac0a0eb04d584a963e7fef4aeeceded4a09
kimdong0619 <ghfkddl9906@ajou.ac.kr>
2024-12-04 13:06:16 +0900

    add front ultra senseor fucrtion

---
+++ src/R5-externel/app.sample/user_test/ultra2.h
@@ -0,0 +1,12 @@
+// ultra2.h
+#ifndef EXTERNAL_ULTRA2_H
+#define EXTERNAL_ULTRA2_H
+
+#define ULTRA2_STK_SIZE               (512UL)
+#include <sal_internal.h>
+
+//void externalFunction(void);
+//static void ConsoleTask(void *pArg);
+void Ultra2_Task(void);
+#endif // EXTERNAL_H
+
